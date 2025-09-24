3c30921bf7d160b4b07f0526114145028d98bb36
sofiacosme <92559989+sofiacosme@users.noreply.github.com>
2025-09-24 11:48:07 -0500

    Update products.js
    Cambio imagenes

--- src/data/products.js
+++ src/data/products.js
@@ -1,82 +1,105 @@
 export const products = [
-    {
-      id: 1,
-      name: "Laptop Gamer Pro",
-      price: 1200,
-      description: "Laptop para gaming de alto rendimiento",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Laptop+Gamer",
-      category: "Tecnología"
-    },
-    {
-      id: 2,
-      name: "Smartphone Elite",
-      price: 800,
-      description: "Teléfono inteligente de última generación",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Smartphone",
-      category: "Tecnología"
-    },
-    {
-      id: 3,
-      name: "Auriculares Bluetooth",
-      price: 150,
-      description: "Auriculares inalámbricos con cancelación de ruido",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Auriculares",
-      category: "Audio"
-    },
-    {
-      id: 4,
-      name: "Teclado Mecánico",
-      price: 200,
-      description: "Teclado mecánico para gamers",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Teclado",
-      category: "Periféricos"
-    },
-    {
-      id: 5,
-      name: "Monitor 4K Ultra",
-      price: 450,
-      description: "Monitor ultra HD 27 pulgadas",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Monitor+4K",
-      category: "Monitores"
-    },
-    {
-      id: 6,
-      name: "Mouse Inalámbrico",
-      price: 80,
-      description: "Mouse ergonómico Bluetooth",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Mouse",
-      category: "Periféricos"
-    },
-    {
-      id: 7,
-      name: "Tablet Pro",
-      price: 350,
-      description: "Tablet para trabajo y entretenimiento",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Tablet",
-      category: "Tecnología"
-    },
-    {
-      id: 8,
-      name: "Impresora Laser 3D",
-      price: 800,
-      description: "Impresora láser multifunción",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Impresora",
-      category: "Oficina"
-    },
-    {
-      id: 9,
-      name: "Impresora Laser 3D Ultimate",
-      price: 1000,
-      description: "Impresora láser multifunción",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Impresora",
-      category: "Oficina"
-    },
-    {
-      id: 10,
-      name: "Impresora Laser",
-      price: 500,
-      description: "Impresora láser multifunción",
-      image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Impresora",
-      category: "Oficina"
-    }
-  ];
+  {
+    id: 1,
+    name: "Laptop Gamer Pro",
+    price: 1200,
+    description: "Laptop para gaming de alto rendimiento",
+    image: "https://images.unsplash.com/photo-1667984461299-91f50389252c?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Tecnología",
+    stock: 15,
+    promo: true
+  },
+  {
+    id: 2,
+    name: "Smartphone Elite",
+    price: 800,
+    description: "Teléfono inteligente de última generación",
+    image: "https://images.unsplash.com/photo-1610945264803-c22b62d2a7b3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Tecnología",
+    stock: 20,
+    promo: false
+  },
+  {
+    id: 3,
+    name: "Auriculares Bluetooth",
+    price: 150,
+    description: "Auriculares inalámbricos con cancelación de ruido",
+    image: "https://plus.unsplash.com/premium_photo-1678099940967-73fe30680949?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Audio",
+    stock: 30,
+    promo: true
+  },
+  {
+    id: 4,
+    name: "Teclado Mecánico",
+    price: 200,
+    description: "Teclado mecánico para gamers",
+    image: "https://plus.unsplash.com/premium_photo-1664194583917-b0ba07c4ce2a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Periféricos",
+    stock: 12,
+    promo: false
+  },
+  {
+    id: 5,
+    name: "Monitor 4K Ultra",
+    price: 450,
+    description: "Monitor ultra HD 27 pulgadas",
+    image: "https://images.unsplash.com/photo-1675151638946-9fbe1c8b55d1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Monitores",
+    stock: 8,
+    promo: true
+  },
+  {
+    id: 6,
+    name: "Mouse Inalámbrico",
+    price: 80,
+    description: "Mouse ergonómico Bluetooth",
+    image: "https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Periféricos",
+    stock: 40,
+    promo: false
+  },
+  {
+    id: 7,
+    name: "Tablet Pro",
+    price: 350,
+    description: "Tablet para trabajo y entretenimiento",
+    image: "https://images.unsplash.com/photo-1628866971124-5d506bf12915?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Tecnología",
+    stock: 25,
+    promo: false
+  },
+  {
+    id: 8,
+    name: "Impresora Laser 3D",
+    price: 800,
+    description: "Impresora láser multifunción",
+    image: "https://plus.unsplash.com/premium_photo-1714599203805-853a72c23f48?q=80&w=1157&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Oficina",
+    stock: 10,
+    promo: true
+  },
+  {
+    id: 9,
+    name: "Impresora Laser 3D Ultimate",
+    price: 1000,
+    description: "Impresora láser multifunción",
+    image: "https://images.unsplash.com/photo-1758387836566-6a342434f5b4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Oficina",
+    stock: 5,
+    promo: false
+  },
+  {
+    id: 10,
+    name: "Impresora Laser",
+    price: 500,
+    description: "Impresora láser multifunción",
+    image: "https://images.unsplash.com/photo-1612815154858-60aa4c59eaa6?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Oficina",
+    stock: 18,
+    promo: false
+  }
+];
+
+
+export default products;
