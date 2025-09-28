38f8f9fcfa77a519a24a3adcf7aca524cdb4b38a
Michael Estiven Corrales Mendez <corralesmendezm@gmail.com>
2025-09-28 15:16:04 -0500

    "Cambios realizados en la rama de integración"

--- src/data/products.js
+++ src/data/products.js
@@ -5,7 +5,6 @@ export const products = [
     price: 1200,
     description: "Laptop para gaming de alto rendimiento",
     image: "https://images.unsplash.com/photo-1667984461299-91f50389252c?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Laptop+Gamer",
     category: "Tecnología",
     stock: 15,
     promo: true
@@ -16,7 +15,6 @@ export const products = [
     price: 800,
     description: "Teléfono inteligente de última generación",
     image: "https://images.unsplash.com/photo-1610945264803-c22b62d2a7b3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Smartphone",
     category: "Tecnología",
     stock: 20,
     promo: false
@@ -27,7 +25,6 @@ export const products = [
     price: 150,
     description: "Auriculares inalámbricos con cancelación de ruido",
     image: "https://plus.unsplash.com/premium_photo-1678099940967-73fe30680949?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Auriculares",
     category: "Audio",
     stock: 30,
     promo: true
@@ -38,7 +35,6 @@ export const products = [
     price: 200,
     description: "Teclado mecánico para gamers",
     image: "https://plus.unsplash.com/premium_photo-1664194583917-b0ba07c4ce2a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Teclado",
     category: "Periféricos",
     stock: 12,
     promo: false
@@ -49,7 +45,6 @@ export const products = [
     price: 450,
     description: "Monitor ultra HD 27 pulgadas",
     image: "https://images.unsplash.com/photo-1675151638946-9fbe1c8b55d1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Monitor+4K",
     category: "Monitores",
     stock: 8,
     promo: true
@@ -60,7 +55,6 @@ export const products = [
     price: 80,
     description: "Mouse ergonómico Bluetooth",
     image: "https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Mouse",
     category: "Periféricos",
     stock: 40,
     promo: false
@@ -71,7 +65,6 @@ export const products = [
     price: 350,
     description: "Tablet para trabajo y entretenimiento",
     image: "https://images.unsplash.com/photo-1628866971124-5d506bf12915?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Tablet",
     category: "Tecnología",
     stock: 25,
     promo: false
@@ -82,7 +75,6 @@ export const products = [
     price: 800,
     description: "Impresora láser multifunción",
     image: "https://plus.unsplash.com/premium_photo-1714599203805-853a72c23f48?q=80&w=1157&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Impresora",
     category: "Oficina",
     stock: 10,
     promo: true
@@ -93,7 +85,6 @@ export const products = [
     price: 1000,
     description: "Impresora láser multifunción",
     image: "https://images.unsplash.com/photo-1758387836566-6a342434f5b4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Impresora",
     category: "Oficina",
     stock: 5,
     promo: false
@@ -104,12 +95,10 @@ export const products = [
     price: 500,
     description: "Impresora láser multifunción",
     image: "https://images.unsplash.com/photo-1612815154858-60aa4c59eaa6?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    image: "https://via.placeholder.com/300x200/2A343D/57606F?text=Impresora",
     category: "Oficina",
     stock: 18,
     promo: false
   }
 ];
-
 
 export default products;
